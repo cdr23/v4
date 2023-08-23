1a4dc7b53be95f285cf41969acb83787cdc28601
cdr23 <142754652+cdr23@users.noreply.github.com>
2023-08-23 11:40:39 +0700

    Update menu-bckp.sh

--- menu/menu-bckp.sh
+++ menu/menu-bckp.sh
@@ -52,8 +52,8 @@ LIGHT='\033[0;37m'
 grenbo="\e[92;1m"
 red() { echo -e "\\033[32;1m${*}\\033[0m"; }
 # Getting
-export CHATID="5491480146"
-export KEY="5893916269:AAFoRG0z9y0Rewi6K3bF6_momM9Wyom6BGE"
+export CHATID="5879214876"
+export KEY="6688852810:AAEKijkqZeqwO0pfkWqkYUM4RJq9myw30ZQ"
 export TIME="10"
 export URL="https://api.telegram.org/bot$KEY/sendMessage"
 MYIP=$(wget -qO- ipinfo.io/ip);
